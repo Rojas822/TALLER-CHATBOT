4fc31c996b3e56589247e1231c39edb5a3933fc7
Rojas822 <rojascristian822@gmail.com>
2023-03-29 21:34:46 -0500

    Initial commit

---
+++ base-baileys-memory/app.js
@@ -0,0 +1,25 @@
+const { createBot,
+     createProvider,
+     createFlow, 
+     addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const WsProvider = require('@bot-whatsapp/provider/baileys')
+const DBProvider = require('@bot-whatsapp/database/mock')
+const inicio = addKeyword('hola').addAnswer('Hola, bienvenido a la veterinaria')
+
+const main = async () => {
+    const adapterDB = new DBProvider()
+    const adapterFlow = new createFlow([inicio])
+    const adapterProvider = new createProvider(WsProvider) 
+
+    createbot(
+        {
+        flow:adapterFlow,
+        provider: adapterProvider,
+        database : adapterDB,
+        }
+    )
+}
+
+main()
