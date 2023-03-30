870f99c099391b5c7d05a731776bd5d1a3f58ab9
Rojas822 <rojascristian822@gmail.com>
2023-03-29 22:19:12 -0500

    main

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -7,7 +7,41 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const WsProvider = require('@bot-whatsapp/provider/baileys')
 const DBProvider = require('@bot-whatsapp/database/mock')
 
+const perro = addKeyword('perro').addAnswer('Que servicio deseas para tu perro',{
+    buttons:[{
+        body:'corte de pelo'
+    },{
+        body:'vacunacion'
+    },{
+        body:'esterilizacion'
+    },{
+        body:'otro'
+    }
+    ]   
+
+})
+
+const gato = addKeyword('gato').addAnswer('Que servicio deseas para tu gato',{
+    buttons:[{
+        body:'vacunacion'
+    },{
+        body:'esterilizacion'
+    },{
+        body:'otro'
+    }
+        ]
+})
 const inicio = addKeyword('hola').addAnswer('Hola, bienvenido a la veterinaria')
+.addAnswer('Que mascota tienes',{
+    buttons:[{
+        body:'perro'
+    },{
+        body:'gato'
+    },{
+        body:'otro'
+    }
+    ]
+}, null,null,[perro,gato])
 
 const main = async () => {
     const adapterDB = new DBProvider()
@@ -22,5 +56,5 @@ const main = async () => {
         }
     )
 }
-
+QRPortalWeb()
 main()
